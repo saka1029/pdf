65e8037c016ba35196d11282addcfb7df801bb6e
saka1029 <saka1029@gmail.com>
2023-04-04 10:40:50 +0900

    Create TestIText.java

---
+++ src/test/java/saka1029/pdf/itext/TestIText.java
@@ -0,0 +1,14 @@
+package saka1029.pdf.itext;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestIText {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
